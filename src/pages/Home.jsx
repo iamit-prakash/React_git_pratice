6c689395a19a9bbb3a749926334df3d2ff5533b9
Amit Prakash <amitprakash0412@gmail.com>
2026-08-07 11:14:22 +0530

    New commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,12 @@ export default function Home(){
         <div className="container py-4">
             <h1> Seven wonders of world</h1>
             <h2>Great wall of china</h2>
+            <h2>Petra</h2>
+            <h2>Christ the Redeemer</h2>
+            <h2>Machu Pichhu</h2>
+            <h2>Chichen Itza</h2>
+            <h2>Colosseum</h2>
+            <h2>Taj Mahal</h2>
         </div>
     )
 }
